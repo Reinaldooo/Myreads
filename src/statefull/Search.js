d2ca96f58fe3c365646dfa16cc25dca88bf6cf08
Reinaldo Trindade <rewifetri@gmail.com>
2018-11-21 17:08:16 -0200

    added real debounce while seaching, with the help of react-debounce-input

--- src/statefull/Search.js
+++ src/statefull/Search.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import * as BooksAPI from '../BooksAPI'
 import SearchShow from '../stateless/SearchShow'
 import { Link } from 'react-router-dom'
 import './App.css'
-import _ from 'lodash'
+import { DebounceInput } from 'react-debounce-input';
 
 class Search extends Component {
   state = {
@@ -14,55 +14,53 @@ class Search extends Component {
     display: undefined
   }
   componentDidMount() {
-    BooksAPI.getAll().then((books) => { 
+    BooksAPI.getAll().then((books) => {
       this.setState({ owned: books })
     })
-  }  
+  }
   updateQuery = (query) => {
-    this.setState({ query: query.trim() })
-    const debounce = _.debounce((query) => { this.updateQueryShow(query) }, 600)  
-    debounce(query) 
-  } 
-  updateQueryShow = (query) => {
-    BooksAPI.search(query, 2).then((books) => { 
-        this.setState({ books: books, load: false })
-    }) 
+    let cleanQuery = query.trim()
+    this.setState({ query: cleanQuery })
+    BooksAPI.search(cleanQuery, 2).then((books) => {
+      this.setState({ books: books || [], load: false })
+    })
   }
   updateSearch = (book, shelf) => {
     this.props.updateBook(book, shelf)
-    if(shelf === "none"){
-      this.setState({ owned: this.state.owned.filter((b) => b.id !== book.id), display: true}) 
-    
+    if (shelf === "none") {
+      this.setState({ owned: this.state.owned.filter((b) => b.id !== book.id), display: true })
+
     } else {
-     book.shelf = shelf
-    this.setState({ owned: this.state.owned.concat([book]), display: true})
+      book.shelf = shelf
+      this.setState({ owned: this.state.owned.concat([book]), display: true })
     }
-    }
-  render() {  
+  }
+  render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
-            <div className="search-books-input-wrapper">
-              <input type="text"
+          <div className="search-books-input-wrapper">
+            <DebounceInput
               placeholder="Search by title or author"
               value={this.state.query}
-              onChange={(event) => this.updateQuery(event.target.value)}
-              />
-            </div>
+              minLength={2}
+              debounceTimeout={500}
+              onChange={(e) => this.updateQuery(e.target.value)} />
+          </div>
         </div>
         <div className="search-books-results">
-            {
-              this.state.books.length > 0 && 
-              <SearchShow 
+          {
+            this.state.books.length > 0 &&
+            <SearchShow
               data={this.state.books}
               updateSearch={this.updateSearch}
               query={this.state.query}
               owned={this.state.owned}
               load={this.state.load}
               display={this.state.display}
-              />
-            }
+            />
+          }
         </div>
       </div>
     )
